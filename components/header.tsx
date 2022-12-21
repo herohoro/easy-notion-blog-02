4baeae87e748ec3d09ae49a2096d98923efafae7
herohoro <mtwd.office.25@gmail.com>
2022-12-22 00:21:14 +0900

    Fix header-titleImage_responsive

--- components/header.tsx
+++ components/header.tsx
@@ -29,7 +29,7 @@ const Header = () => {
             src="/herohoro_title.png"
             width={400}
             height={100}
-            style={{ objectFit: 'contain' }}
+            style={{ objectFit: 'contain', width: '100%' }}
             alt={NEXT_PUBLIC_SITE_TITLE}
           />
         </Link>
