2b3e7f2688bbd222befc5b67a807c22f2b6b3050
otoyo <hiroki.t.1988@gmail.com>
2023-01-02 17:22:29 +0900

    Move path module include to blog-index-cache.js

--- app/server-constants.js
+++ app/server-constants.js
@@ -1,8 +1,6 @@
 /* eslint @typescript-eslint/no-var-requires: 0 */
 // use commonjs so it can be required without transpiling
 
-const path = require('path')
-
 const NOTION_API_SECRET = process.env.NOTION_API_SECRET
 const DATABASE_ID = process.env.DATABASE_ID
 const INDEX_PAGE_ID = process.env.INDEX_PAGE_ID
@@ -12,7 +10,6 @@ const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
 const NEXT_PUBLIC_SITE_TITLE = 'herohoroブログ'
 const NEXT_PUBLIC_SITE_DESCRIPTION =
   '非エンジニアがeasy-notion-blogを通して勉強しながらスキルアップをしていくブログ'
-const BLOG_INDEX_CACHE = path.resolve('.blog_index_data')
 const NUMBER_OF_POSTS_PER_PAGE = 10
 const SEC_NOTION_API_SECRET = process.env.SEC_NOTION_API_SECRET
 const SEC_DATABASE_ID = process.env.SEC_DATABASE_ID
@@ -26,7 +23,6 @@ module.exports = {
   NEXT_PUBLIC_GA_TRACKING_ID,
   NEXT_PUBLIC_SITE_TITLE,
   NEXT_PUBLIC_SITE_DESCRIPTION,
-  BLOG_INDEX_CACHE,
   NUMBER_OF_POSTS_PER_PAGE,
   SEC_NOTION_API_SECRET,
   SEC_DATABASE_ID,
