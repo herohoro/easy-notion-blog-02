a8ade0b8ef96efad523e2934facf2a7d9ab6ed10
herohoro <mtwd.office.25@gmail.com>
2023-02-15 00:51:52 +0900

    parallax page

---
+++ app/spring/page.tsx
@@ -0,0 +1,19 @@
+import MyCompoPalla from './MyCompoPalla'
+import styles from '../../styles/page.module.css'
+
+export const revalidate = 60
+export const dynamic = 'force-dynamic'
+
+const SpringPage = async () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.onlyContent}>
+          <MyCompoPalla />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SpringPage
