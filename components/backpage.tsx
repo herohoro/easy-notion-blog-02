ff3b5861f8c576a04349dd62db6b22b697d0e776
herohoro <mtwd.office.25@gmail.com>
2023-04-25 19:11:52 +0900

    Fix_event-back

--- components/backpage.tsx
+++ components/backpage.tsx
@@ -1,11 +1,6 @@
-'use client'
-
-import React from 'react'
-import { useRouter } from 'next/navigation'
 import styles from '../styles/blog-parts.module.css'
 
 export const BackPageLink = ({ firstPost, posts }) => {
-  const router = useRouter()
   if (!firstPost) return null
   if (posts.length === 0) return null
 
@@ -13,10 +8,16 @@ export const BackPageLink = ({ firstPost, posts }) => {
 
   if (firstPost.Date !== lastPost.Date) return null
 
+  const handleBack = () => {
+    if (typeof window !== 'undefined') {
+      window.history.back()
+    }
+  }
+
   return (
     <div className={styles.nextContainer}>
       <div className={styles.buttonSubContainer}>
-        <a className={styles.backButton} onClick={() => router.back()}>
+        <a className={styles.backButton} onClick={handleBack}>
           {' '}
           ＜ Back{' '}
         </a>
